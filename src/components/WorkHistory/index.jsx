3f0293bf74f0b072939354c9317d7ce38fe0b966
Shou Zhang <shoud.zhang@gmail.com>
2023-02-16 10:10:10 -0800

    refactor

--- src/components/WorkHistory/index.jsx
+++ src/components/WorkHistory/index.jsx
@@ -1,54 +1,62 @@
 import React, { useState, memo, useRef } from 'react';
 import employmentData from '../../static/data/employmentData';
 import Container from './JobContainer';
-import { JobsContainer } from './workhistory.styles';
-import Divider from '../../static/components/Divder';
+import { JobsContainer } from "./workhistory.styles";
 
 function WorkHistory() {
-  const { cultural, volunteer } = employmentData;
+  const { cultural, volunteer, tech } = employmentData;
   const [openAll, setOpenAll] = useState(false);
   const viewElement = useRef(null);
-  const renderJob = (job, i, type, openAll) => {
-    return <Container job={job} type={type} key={i} openAll={openAll} />;
-  };
 
   const handleClick = () => {
-    if (openAll) viewElement.current.scrollIntoView({ behavior: 'smooth' });
+    if (openAll) viewElement.current.scrollIntoView({ behavior: "smooth" });
     setOpenAll((prev) => !prev);
   };
 
   return (
     <div
       style={{
-        display: 'flex',
-        alignItems: 'center',
-        flexDirection: 'column',
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
       }}
     >
       <h1 id="whereibeen" ref={viewElement}>
         Where I've <span />
       </h1>
       <JobsContainer>
-        <div style={{ flexBasis: '50%' }}>
-          <h1 style={{ textAlign: 'center' }}>Worked</h1>
-          {cultural.map((job, i) => renderJob(job, i, 'cultural', openAll))}
-        </div>
-
-        <div style={{ flexBasis: '50%' }}>
-          <h1 style={{ textAlign: 'center' }}>Volunteered</h1>
-
-          {volunteer.map((job, i) => renderJob(job, i, 'volunteer', openAll))}
-          <button
-            onClick={() => {
-              handleClick();
-            }}
-          >
-            {openAll ? '🔺 Collapse All 🔺' : '🔻 Expand All 🔻'}
-          </button>
-        </div>
+        <JobTypeContainer title="Worked" jobsArr={cultural} openAll={openAll} />
+        <JobTypeContainer
+          title="Volunteered"
+          jobsArr={volunteer}
+          openAll={openAll}
+          handleClick={handleClick}
+        />
       </JobsContainer>
     </div>
   );
 }
 
 export default memo(WorkHistory);
+
+const renderJob = (job, i, type, openAll) => {
+  return <Container job={job} type={type} key={i} openAll={openAll} />;
+};
+
+const JobTypeContainer = ({ title, jobsArr, openAll, handleClick }) => {
+  return (
+    <div style={{ flexBasis: "50%" }}>
+      <h1 style={{ textAlign: "center" }}>{title}</h1>
+      {jobsArr.map((job, i) => renderJob(job, i, "cultural", openAll))}
+      {handleClick && (
+        <button
+          onClick={() => {
+            handleClick();
+          }}
+        >
+          {openAll ? "🔺 Collapse All 🔺" : "🔻 Expand All 🔻"}
+        </button>
+      )}
+    </div>
+  );
+};
